66348456787bbf01780c1ebd1f86018b27c25c12
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-05-28 20:09:06 +0200

    Adding a login page

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container> <login-form></login-form></v-container>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import LoginForm from '@/components/auth/LoginForm.vue'
+export default defineComponent({
+  name: 'Login',
+  components: { LoginForm }
+})
+</script>
