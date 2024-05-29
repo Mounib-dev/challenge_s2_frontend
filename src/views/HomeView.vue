662608985a20b14d4594bcb51d40b750b17799bf
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-05-29 20:37:23 +0200

    Feat (not finishe and to improve): adding a snackbar display when a user successefuly authenticate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { useSnackbarStore } from '../stores/snackbar'
+import { useAuthStore } from '@/stores/auth'
+export default {
+  mounted() {
+    const snackbarStore = useSnackbarStore()
+    // console.log(useAuthStore().$state.user.firstname)
+    // const currentUser = useAuthStore().$state.user.firstname
+    // console.log(currentUser)
+    if (useAuthStore().isAuthenticated) {
+      console.log('test mounted')
+      return snackbarStore.showSnackbar(`Welcome, you are now authenticated`)
+    }
+  }
+}
+</script>
 
 <template>
-  <main>
+  <v-main>
     <div>
       <h1>Welcome</h1>
     </div>
-  </main>
+  </v-main>
 </template>
