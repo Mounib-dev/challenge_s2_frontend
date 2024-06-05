3c1eb62e3acf37387071c5ddfc1733a6b7295544
l3miage-larbiy <larbi1992yasmine@gmail.com>
2024-06-05 20:37:46 +0200

    add axios function

---
+++ src/views/Tasks/TasksListView.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-responsive>
+      <tasks-list :tasks="tasks"></tasks-list>
+    </v-responsive>
+  </v-container>
+</template>
+
+
+
+  
+  
+  
+  <script>
+import TasksList from '@/components/Tasks/TasksList.vue'
+
+import axios from 'axios'
+
+export default {
+  components: {
+    TasksList
+  },
+
+  data() {
+    return {
+      tasks: []
+    }
+  },
+
+  mounted() {
+    axios
+      .get('http://localhost:3000/api/v1/tasks')
+      .then((tasks) => {
+        console.log(tasks.data)
+        this.tasks = tasks.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
+</script>
+  
