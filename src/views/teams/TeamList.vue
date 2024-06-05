6d43de67873fc79d9d6a8c6f4c829f84124e9919
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-06-05 18:37:13 +0200

    Minor change: display improvment when a team doesn't have team members yet

--- src/views/teams/TeamList.vue
+++ src/views/teams/TeamList.vue
@@ -16,29 +16,35 @@
         <v-card-text>
           <div><strong>Name :</strong> {{ selectedTeam?.name }}</div>
           <div><strong>Description :</strong> {{ selectedTeam?.description }}</div>
-          <div><strong>Date of creation :</strong> {{ formatDate(selectedTeam?.creationDate) }}</div>
-          <div><strong>Members :</strong></div>
-          <ul>
-            <li v-for="member in selectedTeam?.members" :key="member._id">
-              {{ member.firstname }} {{ member.lastname }}
-            </li>
-          </ul>
+          <div>
+            <strong>Date of creation :</strong> {{ formatDate(selectedTeam?.creationDate) }}
+          </div>
+          <div v-if="selectedTeam.members.length">
+            <div><strong>Members :</strong></div>
+            <ul>
+              <li v-for="member in selectedTeam?.members" :key="member._id">
+                {{ member.firstname }} {{ member.lastname }}
+              </li>
+            </ul>
+          </div>
+          <div v-else>
+            <div><strong>No members yet! click the edit button to construct the team</strong></div>
+          </div>
         </v-card-text>
         <v-card-actions>
-          <router-link :to="'/edit-team/' + selectedTeam?._id" class="v-btn v-btn--secondary">Edit team</router-link>
+          <v-btn link :to="'/edit-team/' + selectedTeam?._id" class="v-btn v-btn--secondary"
+            >Edit team</v-btn
+          >
           <v-btn color="error" @click="confirmDeleteTeam(selectedTeam)">Delete team</v-btn>
           <v-btn color="primary" @click="dialogVisible = false">Close</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
-  
     <v-dialog v-model="confirmDeleteDialogVisible" max-width="500">
       <v-card>
         <v-card-title class="headline">Confirmation</v-card-title>
-        <v-card-text>
-          Do you really want to delete this team?
-        </v-card-text>
+        <v-card-text> Do you really want to delete this team? </v-card-text>
         <v-card-actions>
           <v-btn color="error" @click="deleteTeam(selectedTeam)">yes</v-btn>
           <v-btn color="primary" @click="confirmDeleteDialogVisible = false">No</v-btn>
@@ -49,59 +55,61 @@
 </template>
 
 <script>
-import axios from 'axios';
-import TeamCard from '@/components/Teams/TeamCard.vue';
+import axios from 'axios'
+import TeamCard from '@/components/Teams/TeamCard.vue'
 
 export default {
   components: {
-    TeamCard,
+    TeamCard
   },
   data() {
     return {
       teams: [],
       dialogVisible: false,
       confirmDeleteDialogVisible: false,
-      selectedTeam: null,
-    };
+      selectedTeam: null
+    }
   },
   created() {
-    this.fetchTeams();
+    this.fetchTeams()
   },
   methods: {
     fetchTeams() {
-      axios.get('http://localhost:3000/api/v1/teams')
-        .then(response => {
-          this.teams = response.data;
+      axios
+        .get('http://localhost:3000/api/v1/teams')
+        .then((response) => {
+          this.teams = response.data
         })
-        .catch(error => {
-          console.error('Erreur lors de la récupération des équipes :', error);
-        });
+        .catch((error) => {
+          console.error('Erreur lors de la récupération des équipes :', error)
+        })
     },
     showTeamDetails(team) {
-      this.selectedTeam = team;
-      this.dialogVisible = true;
+      this.selectedTeam = team
+      this.dialogVisible = true
     },
     confirmDeleteTeam(team) {
-      this.selectedTeam = team;
-      this.confirmDeleteDialogVisible = true;
+      this.selectedTeam = team
+      this.confirmDeleteDialogVisible = true
     },
     deleteTeam(team) {
-      axios.delete(`http://localhost:3000/api/v1/teams/delete/${team._id}`)
+      axios
+        .delete(`http://localhost:3000/api/v1/teams/delete/${team._id}`)
         .then(() => {
-          this.teams = this.teams.filter(t => t._id !== team._id);
-          this.confirmDeleteDialogVisible = false;
+          this.teams = this.teams.filter((t) => t._id !== team._id)
+          this.confirmDeleteDialogVisible = false
         })
-        .catch(error => {
-          console.error('Erreur lors de la suppression de l\'équipe :', error);
-          this.confirmDeleteDialogVisible = false;
-        });
+        .catch((error) => {
+          console.error("Erreur lors de la suppression de l'équipe :", error)
+          this.confirmDeleteDialogVisible = false
+        })
     },
     formatDate(dateString) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      return new Date(dateString).toLocaleDateString('en-US', options);
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(dateString).toLocaleDateString('en-US', options)
     }
   }
-};
+}
 </script>
 
 <style scoped>
