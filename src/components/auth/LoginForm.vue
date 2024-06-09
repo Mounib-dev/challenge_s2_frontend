820b5c6423f34f0d734c86a5b475bcbbf45abb88
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-06-09 16:34:35 +0200

    Improve authentication UX

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -2,6 +2,15 @@
   <v-card class="w-75 mx-auto">
     <v-responsive class="mx-auto" max-width="344">
       <v-form @submit.prevent="login">
+        <v-alert
+          v-if="loginResponse"
+          :type="alertType"
+          variant="outlined"
+          class="my-5"
+          :icon="alertIcon"
+        >
+          {{ authResponse }}
+        </v-alert>
         <v-text-field
           hide-details="auto"
           label="Email address"
@@ -30,7 +39,7 @@
 
 <script>
 import { useAuthStore } from '@/stores/auth'
-
+import { useRouter } from 'vue-router'
 export default {
   name: 'LoginForm',
   data() {
@@ -40,14 +49,39 @@ export default {
       rules: {
         required: (value) => !!value || 'Field is required'
       },
-      loading: false
+      loading: false,
+      authResponse: '',
+      loginResponse: false,
+      alertIcon: 'mdi-close-box',
+      alertType: 'error'
     }
   },
   methods: {
     async login() {
       this.loading = true
       const authStore = useAuthStore()
-      await authStore.login({ email: this.username, password: this.password })
+      this.authResponse = await authStore.login({ email: this.username, password: this.password })
+      if (
+        this.authResponse === 'Authentication successful, please wait as you will be redirected'
+      ) {
+        this.alertType = 'success'
+        this.alertIcon = 'mdi-check-circle'
+        this.loginResponse = true
+        return setTimeout(() => {
+          this.loading = false
+          this.$router.push('/')
+        }, 1500)
+      }
+      this.loading = false
+      if (this.authResponse === 'Invalid credentials.') {
+        this.loginResponse = true
+      } else if (this.authResponse === "We couldn't find an account associated with this email.") {
+        this.loginResponse = true
+      } else {
+        this.alertIcon = 'mdi-emoticon-sad-outline'
+        this.loginResponse = true
+      }
+      console.log(authResponse)
     }
   }
 }
