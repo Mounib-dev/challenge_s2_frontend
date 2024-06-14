61ba0267e1512424c83842831c218e63e68a876c
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-06-14 11:30:21 +0200

    Display: dashboard V1 with tasks, teams and employees (team members) counts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,37 @@
+<template>
+  <v-container>
+    <div>
+      <h1>Dashboard stats</h1>
+    </div>
+  </v-container>
+  <v-container>
+    <v-card prepend-icon="mdi-account" :title="`Employees : ${tasksCount}`">
+      <v-card-text>Number of Employees using the workloads planner</v-card-text></v-card
+    >
+    <v-card prepend-icon="mdi-account-group" :title="`Teams : ${teamsCount}`">
+      <v-card-text>Number of Teams</v-card-text></v-card
+    >
+    <v-card prepend-icon="mdi-clipboard-list" :title="`Tasks : ${employeesCount}`">
+      <v-card-text>Total tasks registred in the app</v-card-text></v-card
+    >
+  </v-container>
+</template>
+
 <script lang="ts">
 import { computed, onMounted } from 'vue'
+import axios from 'axios'
 import { useSnackbarStore } from '../stores/snackbar'
 import { useAuthStore } from '@/stores/auth'
-// import { mapState } from 'pinia'
+const tasksCountEndpoint = 'http://localhost:3000/api/v1/tasks?getCount=true'
+const teamsCountEndpoint = 'http://localhost:3000/api/v1/teams?getCount=true'
+const employeesCountEndpoint = 'http://localhost:3000/api/v1/teamMembers?getCount=true'
 export default {
   data() {
     return {
-      previousPage: null
+      previousPage: null,
+      tasksCount: null || Number,
+      teamsCount: null || Number,
+      employeesCount: null || Number
     }
   },
   beforeRouteEnter(to, from, next) {
@@ -34,6 +59,25 @@ export default {
     logout() {
       const authStore = useAuthStore()
       authStore.logout()
+    },
+    async getCounts() {
+      try {
+        const [tasksCount, teamsCount, employeesCount] = await Promise.all([
+          axios.get(tasksCountEndpoint),
+          axios.get(teamsCountEndpoint),
+          axios.get(employeesCountEndpoint)
+        ])
+
+        console.log(tasksCount)
+        console.log(teamsCount)
+        console.log(employeesCount)
+
+        this.tasksCount = tasksCount.data
+        this.teamsCount = teamsCount.data
+        this.employeesCount = employeesCount.data
+      } catch (err) {
+        console.error(err)
+      }
     }
   },
   async mounted() {
@@ -51,16 +95,9 @@ export default {
       console.log('test mounted')
       return snackbarStore.showSnackbar(`Successefuly disconnected!`)
     }
+    this.getCounts()
   }
 }
 </script>
-
-<template>
-  <v-container>
-    <div>
-      <h1>Welcome {{ user.firstname }}</h1>
-    </div>
-  </v-container>
-</template>
 
 <style></style>
